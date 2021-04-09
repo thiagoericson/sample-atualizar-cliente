a503a7729d884230692e7d2d805f6b7b3ae1c430
Thiago Cabral <ericson.thiago@gmail.com>
2021-04-09 16:40:52 -0300

    First commit

---
+++ src/main/java/br/com/treinamento/atualizarcliente/AtualizarClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.atualizarcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtualizarClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtualizarClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinamento/atualizarcliente/AtualizarClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.atualizarcliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtualizarClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
